a44e01200b6d4aed4867cea9b74a27dd3180348d
Takahito Urai <uraitakahito@gmail.com>
2025-01-11 11:43:09 +0900

    feat: rename and restructure packages for importing external ES modules

--- packages/named-exports-no-default/index.ts
+++
@@ -1,2 +0,0 @@
-export const named1 = 'named1';
-export const named2 = 'named2';
--- packages/named-exports-with-default/index.ts
+++
@@ -1,9 +0,0 @@
-//
-// ALERT:
-//   DO NOT USE default export
-//   https://engineering.linecorp.com/ja/blog/you-dont-need-default-export
-//
-export const named3 = 'named3';
-const named4 = 'named4';
-
-export default named4;
